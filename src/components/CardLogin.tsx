42f4f816f979e6235b5f661fb7aec65c8832094e
mechztcc <dojathan@gmail.com>
2023-04-06 17:24:53 -0300

    feat: added react hook forms

--- src/components/CardLogin.tsx
+++ src/components/CardLogin.tsx
@@ -1,32 +1,60 @@
-import React from "react";
 import "./styles.css";
 
-interface CardLoginProps {}
+import { SubmitHandler, useForm } from "react-hook-form";
 
-interface CardLoginState {}
-
-class CardLogin extends React.Component<CardLoginProps, CardLoginState> {
-  render() {
-    return (
-      <div className="card">
-        <h1>Lets login into yout account</h1>
-        <form>
-          <label htmlFor="email">E-mail</label>
-          <input type="text" />
-
-          <label htmlFor="password">Password</label>
-          <input type="password" />
-
-          <button type="submit">Login</button>
-          <span>Forgot password?</span>
-        </form>
-        <div className="card-footer">
-            <small>Dont have account?</small>
-            <span>Register now!</span>
-        </div>
-      </div>
-    );
-  }
+interface IFormInput {
+  email: String;
+  password: String;
 }
 
-export default CardLogin;
+export default function CardLogin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <div className="card">
+      <h1>Lets login into yout account</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          {...register("email", {
+            onChange: () => console.log(errors),
+            required: true,
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Invalid e-mail",
+            },
+          })}
+        />
+        <p>{errors.email?.message}</p>
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          {...register("password", {
+            minLength: 8,
+            maxLength: 10,
+            required: true,
+          })}
+        />
+        <p>
+          {(errors.password?.type === "minLength" && "Password is short") ||
+            (errors.password?.type === "maxLength" && "Password is too big")}
+        </p>
+
+        <button type="submit">Login</button>
+        <span>Forgot password?</span>
+      </form>
+      <div className="card-footer">
+        <small>Dont have account?</small>
+        <span>Register now!</span>
+      </div>
+    </div>
+  );
+}
