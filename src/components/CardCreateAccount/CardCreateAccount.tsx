0c258d6cc78337aaa2ab5c7ea789eb311a43f2fc
mechztcc <dojathan@gmail.com>
2023-04-07 09:18:18 -0300

    feat: added apollo client

--- src/components/CardCreateAccount/CardCreateAccount.tsx
+++ src/components/CardCreateAccount/CardCreateAccount.tsx
@@ -1,13 +1,56 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
+import { gql, useMutation, useQuery } from "@apollo/client";
+
 import "./styles.css";
+import { useState, useEffect } from "react";
 
 interface IFormInput {
-  name: String;
-  email: String;
-  password: String;
+  name: string;
+  secondName: string;
+  email: string;
+  password: string;
+  vip: boolean;
+  roleId: number;
+  payment: string;
+  birth: string;
 }
+
+const CREATE_ACCOUNT = gql`
+  mutation Create(
+    $email: String!
+    $password: String!
+    $name: String!
+    $second_name: String!
+    $payment: String!
+    $birth: String!
+    $role: Int!
+  ) {
+    create(
+      email: $email
+      password: $password
+      name: $name
+      second_name: $second_name
+      payment: $payment
+      birth: $birth
+      role: $role
+    ) {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const FIND_ROLES = gql`
+  query Roles {
+    roles {
+      id
+      name
+    }
+  }
+`;
 
 export default function CardCreateAccount() {
   const {
@@ -15,7 +58,24 @@ export default function CardCreateAccount() {
     handleSubmit,
     formState: { errors, isDirty },
   } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    await createUser();
+  };
+
+  const [createUser] = useMutation(CREATE_ACCOUNT, {
+    variables: {
+      email: "email2@email.com",
+      password: "1234567",
+      name: "name",
+      second_name: "name",
+      payment: "200",
+      birth: "22/10/2023",
+      role: 1,
+      vip: true,
+    },
+    onError: console.log,
+  });
 
   return (
     <div className="card">
@@ -29,7 +89,43 @@ export default function CardCreateAccount() {
           })}
         />
 
-        {(errors.name && isDirty) && <span role="alert">Name is required</span>}
+        {errors.name && isDirty && <span role="alert">Name is required</span>}
+
+        <label htmlFor="name">Second name</label>
+        <input
+          type="text"
+          {...register("secondName", {
+            required: true,
+          })}
+        />
+
+        {errors.secondName && isDirty && (
+          <span role="alert">Second name is required</span>
+        )}
+
+        <label htmlFor="name">Payment date</label>
+        <input
+          type="text"
+          {...register("payment", {
+            required: true,
+          })}
+        />
+
+        {errors.payment && isDirty && (
+          <span role="alert">Payment date is required</span>
+        )}
+
+        <label htmlFor="name">Birth date</label>
+        <input
+          type="text"
+          {...register("payment", {
+            required: true,
+          })}
+        />
+
+        {errors.birth && isDirty && (
+          <span role="alert">Birth date is required</span>
+        )}
 
         <label htmlFor="email">E-mail</label>
         <input
@@ -43,7 +139,7 @@ export default function CardCreateAccount() {
             },
           })}
         />
-        {(errors.email && isDirty) && <span role="alert">Invalid e-mail</span>}
+        {errors.email && isDirty && <span role="alert">Invalid e-mail</span>}
 
         <label htmlFor="password">Password</label>
         <input
